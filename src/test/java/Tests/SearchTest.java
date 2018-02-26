edfb869e965265d7a70c6024a766e15a225f0bb7
daavi <daavi@LAPTOP-OFFDMLM9>
2018-02-26 07:56:11 +0100

    2

--- src/test/java/Tests/SearchTest.java
+++ src/test/java/Tests/SearchTest.java
@@ -14,7 +14,7 @@ import Pages.SearchPage;
 public class SearchTest extends DriverManager {
 	
 	@Test
-	public void serach() throws MalformedURLException {
+	public void serach_chrome() throws MalformedURLException {
 		
 		System.out.println("Test is started");
 		driver = new DriverManager().getDriver("chrome");
@@ -22,6 +22,38 @@ public class SearchTest extends DriverManager {
 		SearchPage searchPage = new SearchPage(driver);
 		assertTrue(searchPage.isInitialized());
 		searchPage.enterSearch("Dawid");
+		searchPage.submit();
+		
+		assertTrue(searchPage.isInitialized());
+		assertTrue(searchPage.confirmationHeader().contains("wikipedia"));
+		System.out.println("Test is finished");
+	}
+	
+	@Test
+	public void serach_chrome2() throws MalformedURLException {
+		
+		System.out.println("Test is started");
+		driver = new DriverManager().getDriver("chrome");
+		driver.get("http://www.google.pl");
+		SearchPage searchPage = new SearchPage(driver);
+		assertTrue(searchPage.isInitialized());
+		searchPage.enterSearch("Marcin");
+		searchPage.submit();
+		
+		assertTrue(searchPage.isInitialized());
+		assertTrue(searchPage.confirmationHeader().contains("wikipedia"));
+		System.out.println("Test is finished");
+	}
+	
+	@Test
+	public void serach_firefox() throws MalformedURLException {
+		
+		System.out.println("Test is started");
+		driver = new DriverManager().getDriver("firefox");
+		driver.get("http://www.google.pl");
+		SearchPage searchPage = new SearchPage(driver);
+		assertTrue(searchPage.isInitialized());
+		searchPage.enterSearch("Rafa³");
 		searchPage.submit();
 		
 		assertTrue(searchPage.isInitialized());
